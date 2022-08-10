d9749941fadf417c574960043cf5366f0fdda0ef
Brian Hasal <brian.hasal@gmail.com>
2022-08-10 04:38:44 -0700

    React8

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navbartest = (
+  <nav>
+    <h1>Chewbacca Enterprises</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(navbartest, document.getElementById("root"));
+
+
+
+
+
+
+
+
 // ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
 
 /* 
@@ -11,10 +50,10 @@ appending an h1 to our div#root (without using innerHTML).
     
 */
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is the imperative way of coding"
-h1.className = "header"
-document.getElementById("root").append(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is the imperative way of coding"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
 
 
 

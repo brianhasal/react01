e7f508577e621ba051e1abade54c1ba818ebd936
Brian Hasal <brian.hasal@gmail.com>
2022-08-10 05:12:13 -0700

    React 11 - Thought experiment re: JavaScript .append()

--- index.js
+++ index.js
@@ -1,9 +1,73 @@
 
 
 
-// If I remove the react calls in index.html, I have to import React and ReactDOM here
-import React from "react"
-import ReactDOM from "react-dom"
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// /**
+// Challenge: fix our code!
+
+// Don't forget, you're not using CDNs anymore, so there's no
+// global "ReactDOM" variable to use anymore.
+//  */
+
+// const page = (
+//     <div>
+//         <h1>My awesome website in React</h1>
+//         <h3>Reasons I love React</h3>
+//         <ol>
+//             <li>It's composable</li>
+//             <li>It's declarative</li>
+//             <li>It's a hireable skill</li>
+//             <li>It's actively maintained by skilled people</li>
+//         </ol>
+//     </div>
+// )
+
+// ReactDOM.render(page, document.getElementById("root"))
+
+
+// import React from "react"
+
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
+
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
+
+
+// const newPage = (
+//   <div className="root">
+//       <h1>Hello</h1>
+//       <p>Things in a paragraph</p>
+//       <h2>Sub Title</h2>
+//       <p>Things in a paragraph</p>
+//   </div>
+// )
+
+// document.getElementById("root").append(JSON.stringify(newPage))
+
+
+
+// // If I remove the CDNs in index.html, I have to import React and ReactDOM here
+// import React from "react"
+// import ReactDOM from "react-dom"
 
 
 // const page = (
@@ -25,18 +89,18 @@ Create a navbar in JSX:
     - Don't worry about styling yet - it'll just be plain-looking HTML for now
 */
 
-const navbar = (
-  <nav>
-    <h1>Chewbacca Enterprises</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-)
+// const navbar = (
+//   <nav>
+//     <h1>Chewbacca Enterprises</h1>
+//     <ul>
+//       <li>Pricing</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   </nav>
+// )
 
-ReactDOM.render(navbar, document.getElementById("root"));
+// ReactDOM.render(navbar, document.getElementById("root"));
 
 
 

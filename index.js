906cdba4dc4b5c89d55fb713edc604ab50c85ed8
Brian Hasal <brian.hasal@gmail.com>
2022-08-10 07:11:33 -0700

    Finished 17, complicated to save in two places

--- index.js
+++ index.js
@@ -1,39 +1,132 @@
-/**
-Challenge: 
-
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
-
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Page() {
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function Header() {
   return (
-    <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" width="40px" />
-        </nav>
-      </header>
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+    <>
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
         <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
-      <footer>© 2022 Hasal development. All rights reserved.</footer>
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
 
 ReactDOM.render(<Page />, document.getElementById("root"));
+
+
+
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <h1>Reasons I'ma excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2021 Ziroll development. All rights reserved.</small>
+      </footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
+
+// /**
+// Challenge:
+
+// Part 2:
+// - Add a `header` element with a nested `nav` element. Inside the `nav`,
+//   include a `img` element with the image of the React logo inside
+//   (src="./react-logo.png") and make sure to set the width to something
+//   more manageable so it doesn't take up the whole screen
+// - Add an `h1` with some text describing the page. (E.g. "Reasons
+//   I'm excited to learn React"). Place it above the ordered list.
+// - Add a `footer` after the list that says:
+//     "© 20xx <last name here> development. All rights reserved."
+//  */
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// function Page() {
+//   return (
+//     <div>
+//       <header>
+//         <nav>
+//           <img src="./react-logo.png" width="40px" />
+//         </nav>
+//       </header>
+//       <h1>Reasons I'm excited to learn React</h1>
+//       <ol>
+//         <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+//         <li>I'm more likely to get a job as a developer if I know React</li>
+//       </ol>
+//       <footer>© 2022 Hasal development. All rights reserved.</footer>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"));
 
 // /**
 // Challenge:

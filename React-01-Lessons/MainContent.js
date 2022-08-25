1f646fd73149bac3da49ddff7fd9b5ca5de11722
Brian Hasal <brian.hasal@gmail.com>
2022-08-25 07:04:57 -0700

    CD

--- React-01-Lessons/MainContent.js
+++ React-01-Lessons/MainContent.js
@@ -8,7 +8,7 @@ export default function MainContent() {
                 <li>It's a popular library, so I'll be 
                 able to fit in with the cool kids!</li>
                 <li>I'm more likely to get a job as a developer
-                if I know React</li>
+                if I know React!</li>
             </ol>
         </div>
     )
